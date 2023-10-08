dd3c6482176c4f515ac5bfecb1a1488847f88a5c
THCode <qnprk55@gmail.com>
2023-10-09 00:59:44 +0900

    game make end

--- copy/game/shooting.js
+++ copy/game/shooting.js
@@ -1,6 +1,8 @@
 window.onload = function() {
   let canvas
   let ctx
+  let gameOver=false
+  let score = 0
 
   canvas = document.createElement('canvas')
   ctx = canvas.getContext('2d')
@@ -13,6 +15,62 @@ window.onload = function() {
   let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage
   let spaceshipX = canvas.width/2 - 32
   let spaceshipY = canvas.height - 120
+
+  let bulletList = []
+  
+  function Bullet() {
+    this.x = 0
+    this.y = 0
+    this.init = function () {
+      this.x = spaceshipX + 10
+      this.y = spaceshipY - 20
+      this.alive = true
+
+      bulletList.push(this)
+    }
+
+    this.update = function() {
+      this.y -= 7
+    }
+
+    this.checkHit = function() {
+      for (let i = 0; i < enemyList.length; i++) {
+        if(this.y <= enemyList[i].y && 
+          this.x >= enemyList[i].x && 
+          this.x <= enemyList[i].x + 40) {
+          score++
+          this.alive = false
+          enemyList.splice(i, 1)
+        }
+      }
+    }
+  }
+
+  function generateRandomValue(min, max) {
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+    return randomNum
+  }
+
+  let enemyList = []
+
+  function Enemy() {
+    this.x = 0
+    this.y = 0
+    this.init = function() {
+      this.y = 0
+      this.x = generateRandomValue(0, canvas.width-48)
+      enemyList.push(this)
+    }
+    
+    this.update = function() {
+      this.y += 2
+      if (this.y >= canvas.height - 48) {
+        gameOver = true
+        console.log('gameover')
+      }
+    }
+
+  }
 
   function loadImage() {
     backgroundImage = new Image()
@@ -32,12 +90,27 @@ window.onload = function() {
   function setupKeyboardListener() {
     document.addEventListener('keydown', function(event) {
       keysDown[event.key] = true
-      console.log(keysDown)
     })
     document.addEventListener('keyup', function(event) {
       delete keysDown[event.key]
-      console.log(keysDown)
+      
+      if (event.key == ' ') {
+        createBullet()
+        console.log('bullet')
+      }
     })
+  }
+
+  function createBullet() {
+    let b = new Bullet()
+    b.init()
+  }
+
+  function createEnemy() {
+    const interval = setInterval(function() {
+      let e = new Enemy()
+      e.init()
+    }, 1)
   }
 
   function update() {
@@ -90,20 +163,52 @@ window.onload = function() {
     if (spaceshipY >= canvas.height - 76) {
       spaceshipY = canvas.height - 76
     }
+
+    for (let i = 0; i < bulletList.length; i++) {
+      if (bulletList[i].alive) {
+        bulletList[i].update()
+        bulletList[i].checkHit()
+      }
+    }
+
+    for (let i = 0; i < enemyList.length; i++) {
+      enemyList[i].update()
+    }
+
+
   }
 
   function render() {
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height)
     ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY)
+    ctx.fillText(`Score: ${score}`, 20, 40)
+    ctx.fillStyle = 'white'
+    ctx.font = '16pt Arial'
+
+    for (let i = 0; i < bulletList.length; i++) {
+      if (bulletList[i].alive) {
+        ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y)
+      }
+    }
+
+    for (let i = 0; i < enemyList.length; i++) {
+      ctx.drawImage(enemyImage, enemyList[i].x, enemyList[i].y)
+    }
   }
 
   function main() {
-    update()
-    render()
-    requestAnimationFrame(main)
+    if(!gameOver) {
+      update()
+      render()
+      requestAnimationFrame(main)
+    }
+    else {
+      ctx.drawImage(gameOverImage, 10, 100, 380, 380)
+    }
   }
 
   loadImage()
   setupKeyboardListener()
+  createEnemy()
   main()
 }
