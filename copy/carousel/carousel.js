55d99d6179a74c835970866ba47f5b03efe321e3
THCode <qnprk55@gmail.com>
2023-10-22 23:24:57 +0900

    123

--- copy/carousel/carousel.js
+++ copy/carousel/carousel.js
@@ -1,9 +1,9 @@
 const carousel = document.querySelector('.carousel'),
-firstImg = carousel.querySelectorAll('.img')[0]
+firstImg = carousel.querySelectorAll('.img')[0],
 arrowIcons = document.querySelectorAll('.wrapper .i')
 
 let isDragStart = false, prevPageX, prevScrollLeft
-
+console.log(isDragStart)
 const showHideIcons = () => {
   let scrollWidth = carousel.scrollWidth - carousel.clientWidth
   arrowIcons[0].style.display = carousel.scrollLeft == 0 ? 'none' : 'block'
