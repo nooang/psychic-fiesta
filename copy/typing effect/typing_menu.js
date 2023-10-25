8a523baab35085fe0fbea3f2c12a8a1b6182beea
THCode <qnprk55@gmail.com>
2023-10-26 00:58:16 +0900

    cx

--- copy/typing effect/typing_menu.js
+++ copy/typing effect/typing_menu.js
@@ -1,3 +1,13 @@
-const content = 'Typing Effect'
+const content = 'I Love My Cat'
 const text = document.querySelector('.text')
 let index = 0
+
+function typing() {
+  text.textContent += content[index++]
+  if (index > content.length) {
+    text.textContent = ""
+    index = 0
+  }
+}
+
+setInterval(typing, 300)
