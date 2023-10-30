2b27a5faad4f8c1bec9b02b077490b6f091e02b9
THCode <qnprk55@gmail.com>
2023-10-30 23:48:01 +0900

    asdasdasdasd
    asdasda

--- copy/particle/main_error.js
+++ copy/particle/main_error.js
@@ -1,4 +1,4 @@
-import utils from './utils.js'
+import util from './utils.js'
 
 // 1. 캔버스 불러오기
 const canvas = document.querySelector('canvas')
@@ -25,22 +25,27 @@ class Particle {
 
   animate() {
     if (this.x < 0 || this.x > innerWidth || this.y < 0 || this.y > innerHeight) {
-      this.x = utils.randomFloatBetween(0, innerWidth)
-      this.y = utils.randomFloatBetween(0, innerHeight)
+      this.x = util.randomFloatBetween(0, innerWidth)
+      this.y = util.randomFloatBetween(0, innerHeight)
     }
 
     (mouse.isActive ? particles.concat(mouse) : particles).forEach(particle => {
-      const distance = utils.distance(particle.x, particle.y, this.x, this.y)
-      if (distance < 200) {
-        const from = { x: this.x, y: this.y }
-        const to = { x: particle.x, y: particle.y }
+      const distance = util.distance(particle.x, particle.y, this.x, this.y)
+      if (distance < 400) {
+        const from = {
+          x: this.x,
+          y: this.y
+        }
+        const to = {
+          x: particle.x,
+          y: particle.y
+        }
         new Line(from, to, distance).draw()
       }
     })
 
     this.x += this.velocity.x
     this.y += this.velocity.y
-
     this.draw()
   }
 }
@@ -51,28 +56,28 @@ class Line {
     this.to = to
     this.distance = distance
   }
-
   draw() {
     ctx.beginPath()
-    ctx.moveTo(this.from.x, this.from.y)
+    ctx.moveTo(this.to.x, this.to.y)
     ctx.lineTo(this.to.x, this.to.y)
-    ctx.strokeStyle = `rgba(255, 215, 0, ${1- this.distance / 200})`
+    ctx.strokeStyle = `rgba(255, 215, 0, ${1 - this.distance / 400})`
     ctx.lineWidth = 1
     ctx.stroke()
   }
 }
 
+
 // 3. 파티클 만들기
-const TOTAL = 150
+const TOTAL = 50
 let particles = []
 let mouse = { x: 0, y: 0, isActive: false }
 for (let i = 0; i < TOTAL; i++) {
-  const x = utils.randomFloatBetween(0, innerWidth)
-  const y = utils.randomFloatBetween(0, innerHeight)
-  const radius = utils.randomFloatBetween(0.5, 2)
+  const x = util.randomFloatBetween(0, innerWidth)
+  const y = util.randomFloatBetween(0, innerHeight)
+  const radius = util.randomFloatBetween(0.5, 2)
   const velocity = {
-    x: utils.randomFloatBetween(-2, 2),
-    y: utils.randomFloatBetween(-2, 2),
+    x: util.randomFloatBetween(-2, 2),
+    y: util.randomFloatBetween(-2, 2)
   }
   particles.push(new Particle(x, y, radius, velocity))
 }
@@ -80,9 +85,7 @@ for (let i = 0; i < TOTAL; i++) {
 // 4. 매 프레임마다 실행되는 재귀함수
 function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-
   particles.forEach(particle => particle.animate())
-
   window.requestAnimationFrame(render)
 }
 
@@ -94,8 +97,8 @@ window.addEventListener('resize', () => {
 canvas.addEventListener('mouseenter', () => mouse.isActive = true)
 canvas.addEventListener('mouseleave', () => mouse.isActive = false)
 canvas.addEventListener('mousemove', e => {
-  mouse.x = e.clientX
-  mouse.y = e.clientY
+  mouse.e = e.clientX
+  mouse.e = e.clientY
 })
 
 // 6. 실행하기
